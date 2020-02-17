2b702b72bd864acf048d6a7debaa38aa49b7fd92
Simon <m-tabola@mail.ru>
2020-02-17 20:39:08 +0300

    add new functions in addCustFunc

--- addCustFunc/addCustFunc.c
+++ addCustFunc/addCustFunc.c
@@ -14,18 +14,54 @@ int* getRandFillArr(size_t size, int min, int max) {
 
     int* sortArr = malloc(sizeof(int) * size);
 
+    assert(!(sortArr == NULL));
+
     for (int i = 0; i < size; i++)
     {
         sortArr[i] = min + (rand() % (max - min));
     }
-
     return sortArr;
 }
 
 
-void exchangeElements(int* arr, size_t i, size_t j)
+void exchangeElements(int* arr, int i, int j)
 {
+    if (i == j)
+        return;
+
     int tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
+}
+
+int findMaxElem(int* arr, size_t arrSize)
+{
+    int maxElem = arr[0];
+    for (size_t i = 1; i < arrSize; i++)
+        if (arr[i] > maxElem)
+            maxElem = arr[i];
+    return maxElem;
+}
+
+
+int findMaxGrade(int* inpArr, size_t size)
+{
+    int i = 0;
+
+    while (true)
+    {
+        int zeroCounter = 0;
+        int currentGrade = (int)pow(10, i);
+
+        for (int j = 0; j < size; j++)
+        {
+            if ((inpArr[j] / currentGrade) == 0)
+                zeroCounter++;
+        }
+        if (zeroCounter == size)
+            break;
+        i++;
+    }
+
+    return i - 1;
 }
--- addCustFunc/addCustFunc.h
+++ addCustFunc/addCustFunc.h
@@ -2,7 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <math.h>
 
 void printArr(char* txt, int* arr, size_t arrSize);
 int* getRandFillArr(size_t size, int min, int max);
-void exchangeElements(int* arr, size_t i, size_t j);
+void exchangeElements(int* arr, int i, int j);
+int findMaxElem(int* arr, size_t arrSize);
+int findMaxGrade(int* inpArr, size_t size);
